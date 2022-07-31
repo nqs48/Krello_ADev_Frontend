d25cb6ef2b434a3682951f28cce490c5a3e017c8
Nestor Quiroga Suárez <nqs48@icloud.com>
2022-07-31 17:42:12 -0500

    Add Action bar to board view

--- frontend/source/javascript/view/update.view.mjs
+++ frontend/source/javascript/view/update.view.mjs
@@ -22,6 +22,33 @@ export class UpdateView {
 
   init() {
 
+    const actionBoardBar = document.createElement("div");
+    actionBoardBar.classList.add("action-board-bar");
+
+    //formulario readonly
+    const boardName = document.createElement("input");
+    boardName.classList.add("board-name");
+    boardName.type = "text";
+    boardName.value = "Title Board";
+
+    // const buttonUpdate = document.createElement("button");
+    // buttonUpdate.classList.add("btn", "btn-primary", "btn-bar");
+    // buttonUpdate.textContent = "Update";
+    // buttonUpdate.addEventListener("click", function () {
+    //   console.log("Update board");
+    // });
+
+    const buttonDelete = document.createElement("button");
+    buttonDelete.classList.add("btn", "btn-danger", "btn-bar");
+    buttonDelete.textContent = "Delete";
+    buttonDelete.addEventListener("click", function () {
+      console.log("Delete board");
+    });
+
+    //Asignando elementos a contenedor
+    actionBoardBar.append(boardName, buttonDelete);
+
+
     console.log(this.privateData)
     const boardColumn = new BoardColumn();
 
@@ -33,7 +60,7 @@ export class UpdateView {
 
 
     this.#privateContainer.append(
-      this.#privateNavbar.get(),
+      this.#privateNavbar.get(),actionBoardBar,
       this.containerColumn.get()
     );
   }
